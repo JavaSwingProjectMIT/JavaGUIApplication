3d66ce922dda89bd67c16e4071a735672183ebf8
NMarkovic98 <pomocnih@gmail.com>
2021-11-24 21:19:12 +0100

    test

--- Super Market Billing System/src/Login.java
+++ Super Market Billing System/src/Login.java
@@ -16,23 +16,15 @@ import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-
-
-
-
 public class Login implements ActionListener,MouseListener{
+	//Initialing DB connection to null
 	private Connection con = null;
 	private Statement stmt = null;
 	private ResultSet rs = null;
-
-	
-
-
-	
+	//Creating needed object for frame
 	JFrame login = new JFrame();
     JButton btnLogin = new JButton("Login");	
     JLabel label = new JLabel();
@@ -45,11 +37,11 @@ public class Login implements ActionListener,MouseListener{
 	JLabel lblNewLabel = new JLabel("Super Market Billing System");
 	JLabel lblRegister = new JLabel("Register");
 
-	
 	Login() throws SQLException{
+		//////////////////////////
+		//Designing frame
 		
-	
-		
+		//Setting login frame
 		login.setResizable(false);
 		login.setVisible(true);
 		login.setForeground(Color.WHITE);
@@ -57,60 +49,47 @@ public class Login implements ActionListener,MouseListener{
 		login.setLayout(null);		
 		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		login.setBounds(100, 100, 500, 500);
-		
-		
-		
-		
-		
-		
-		
+		//Username label		
 		lblusername.setFont(new Font("Ubuntu Condensed", Font.BOLD, 16));
 		lblusername.setForeground(new Color(50, 205, 50));
 		lblusername.setBounds(158, 169, 138, 15);
 		login.add(lblusername);
-		
-		
+		//Username field
 		textFieldUsername.setBounds(158, 196, 220, 23);
 		login.add(textFieldUsername);
 		textFieldUsername.setColumns(10);
-		
-		
+		//Password label
 		lblPassword.setFont(new Font("Ubuntu Condensed", Font.BOLD, 16));
 		lblPassword.setForeground(new Color(50, 205, 50));
 		lblPassword.setBounds(158, 231, 97, 15);
 		login.add(lblPassword);
-		
-		
+		//Password field
 		passwordField.setBounds(158, 256, 220, 23);
 		login.add(passwordField);
-		
-		
+		//Rememmber me checkbox
 		chckbxNewCheckBox.setForeground(Color.WHITE);
 		chckbxNewCheckBox.setBackground(Color.DARK_GRAY);
 		chckbxNewCheckBox.setBounds(158, 287, 167, 23);
 		login.add(chckbxNewCheckBox);
-		
-		
+		//Have no account label
 		lblHaveNoAccount.setForeground(Color.WHITE);
 		lblHaveNoAccount.setBounds(158, 376, 128, 15);
 		login.add(lblHaveNoAccount);
-		
+		//Register label and event on that
 		lblRegister.addMouseListener((MouseListener) this);
 		lblRegister.setForeground(new Color(50, 205, 50));
 		lblRegister.setBounds(290, 376, 70, 15);
 		login.add(lblRegister);
-		
-		
+		//Title label
 		lblNewLabel.setForeground(new Color(50, 205, 50));
 		lblNewLabel.setFont(new Font("Ubuntu Condensed", Font.BOLD, 25));
 		lblNewLabel.setBounds(120, 101, 349, 25);
 		login.add(lblNewLabel);
-		
-		
+		//Logo label
 		label.setIcon(new ImageIcon("/home/nmarkovic/Downloads/green-power.png"));
 		label.setBounds(216, 12, 70, 64);
 		login.add(label);
-		
+		//Login button
 		btnLogin.addActionListener((ActionListener) this);
 		btnLogin.setForeground(new Color(50, 205, 50));
 		btnLogin.setBackground(new Color(255, 255, 255));
@@ -120,7 +99,8 @@ public class Login implements ActionListener,MouseListener{
 		login.add(btnLogin);
 		
 	}
-
+	//////////////////////////
+	//Event Listeners
 	@Override
 	//Manage login click
 	public void actionPerformed(ActionEvent e) {
@@ -149,7 +129,7 @@ public class Login implements ActionListener,MouseListener{
 			
 		}
 	}
-
+	//Manage register clcik
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
